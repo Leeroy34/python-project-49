b29a10d4b2da18673aca265aae71e5f262bc1717
Ларионов Даниил <larionovw68@gmail.com>
2023-04-03 10:19:23 +0300

    Makefile was added

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def main():
+    print('''poetry run python -m brain_games.scripts.brain_games
+Welcome to the Brain Games!''')
+
+
+if __name__ == '__main__':
+    main()
