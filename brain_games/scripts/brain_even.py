23bf3bbaa171e632d38d89f83723b55603f8384c
Ларионов Даниил <larionovw68@gmail.com>
2023-04-04 09:26:57 +0300

    refactor for brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -7,31 +7,22 @@ def main():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    while i < 3:
+
+    for i in range(3):
         number = random.randint(0, 100)
         print(f'Question: {number}')
         answer = prompt.string('Your answer: ')
-        if number % 2 == 0:
-            if answer == 'yes' or answer == 'Yes':
-                print('Correct!')
-            else:
-                print(
-                    f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
-                print(f"Let's try again, {name}")
-                break
-        else:
-            if answer == 'no' or answer == 'No':
-                print('Correct!')
-            else:
-                print(
-                    f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
-                print(f"Let's try again, {name}")
-                break
-        i += 1
 
-    if i == 3:
-        print(f"Congratulations, {name}!")
+        if (number % 2 == 0 and answer.lower() == 'yes') or \
+           (number % 2 != 0 and answer.lower() == 'no'):
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was "
+                  f"'{'yes' if number % 2 == 0 else 'no'}'.")
+            print(f"Let's try again, {name}")
+            return
+
+    print(f"Congratulations, {name}!")
 
 
 if __name__ == '__main__':
