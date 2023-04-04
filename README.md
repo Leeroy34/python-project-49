8c6a21b66ea85dc1f361399d60ce23d98cec8f3e
Ларионов Даниил <larionovw68@gmail.com>
2023-04-04 09:05:15 +0300

    brain_even.py was fixed

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 <a href="https://codeclimate.com/github/Leeroy34/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/a3c728283fd6ac493fed/maintainability" /></a>
 
 link for asciinema:
-https://asciinema.org/a/YTfoICSFCcHjlcGlzHisFrHJu
+https://asciinema.org/a/9g6FfTUZzRX0bG3BDtzv80AIC
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,28 +2,36 @@ import prompt
 import random
 
 
-def welocome_user():
+def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-
-
-def main():
-    welocome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    number = random.randint(0, 100)
-    print(f'Question: {number}')
-    answer = prompt.string('Your answer: ')
-    if number % 2 == 0:
-        if answer == 'yes' or answer == 'Yes':
-            print('Correct!')
+    i = 0
+    while i < 3:
+        number = random.randint(0, 100)
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+        if number % 2 == 0:
+            if answer == 'yes' or answer == 'Yes':
+                print('Correct!')
+            else:
+                print(
+                    f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
+                print(f"Let's try again, {name}")
+                break
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
-    else:
-        if answer == 'no' or answer == 'No':
-            print('Correct!')
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+            if answer == 'no' or answer == 'No':
+                print('Correct!')
+            else:
+                print(
+                    f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+                print(f"Let's try again, {name}")
+                break
+        i += 1
+
+    if i == 3:
+        print(f"Congratulations, {name}!")
 
 
 if __name__ == '__main__':
