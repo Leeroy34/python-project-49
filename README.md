e363489a0b5c07bdca2366f2f654053e089750c7
Ларионов Даниил <larionovw68@gmail.com>
2023-04-04 13:31:44 +0300

    brain_progression was added

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ https://asciinema.org/a/KsToGGVajqcEBNuH8EpXuPxKL
 
 link on asciinema for brain-gcd:
 https://asciinema.org/a/zdwk3W9GlhJ2K7ifR4hFkaJo4
+
+link on asciinema for brain-progression:
+https://asciinema.org/a/IXahWJWU1s4KcG3UirYnHOI2C
---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,53 @@
+import prompt
+import random
+from brain_games.scripts.greet import greet
+
+
+# def progression():
+#     progression = []
+#     number1 = random.randint(1, 100)
+#     number2 = random.randint(1, 10)
+#     number3 = random.randint(1, 10)
+
+#     progression.append(number1)
+#     for i in range(10):
+#         number1 += number2
+#         progression.append(number1)
+
+#     answer = progression[number3]
+#     progression[number3] = '..'
+
+
+def main():
+    name = greet()
+    print('What number is missing in the progression?')
+    for i in range(3):
+        progression = []
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 10)
+        number3 = random.randint(1, 10)
+
+        progression.append(number1)
+        for i in range(10):
+            number1 += number2
+            progression.append(number1)
+
+        answer = progression[number3]
+        progression[number3] = '..'
+
+        print(progression)
+        user_answer = prompt.string('Your answer: ')
+
+        if int(user_answer) == answer:
+            print('Correct!')
+        else:
+            print(
+                f"'{user_answer}' is wrong answer ;(. Correct answer was {answer}")
+            print(f"Let's try again, {name}")
+            return
+
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
